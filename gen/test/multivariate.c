9a57c3dee0419699b49857463110a253c5ef8703
Tobenna Peter Igwe <ptigwe@gmail.com>
2015-02-13 17:21:31 +0000

    Added test for multivariate distribution

---
+++ gen/test/multivariate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../matrix.h"
+#include "../util.h"
+
+int main(int argc, char **argv)
+{
+    matrix_t *cov = matrix_alloc(2, 2);
+    cov->data[0][1] = cov->data[1][0] = atof(argv[1]);
+    cov->data[0][0] = cov->data[1][1] = 1;
+
+    matrix_print(matrix_multivariate_multiple(cov, atoi(argv[2]), atoi(argv[3])));
+
+    return 0;
+}
